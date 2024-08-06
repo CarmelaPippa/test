d9eeb5c82c54c8bb4175ceb6d373dd3d6a37c136
Carmela Pippa <carmelapippa@gmail.com>
2024-08-06 18:01:19 +0200

    script

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let btnEl = document.querySelector("button");
 
 // btnEl.addEventListener("click", function () {
-//   console.log("Button clicked");
-//   window.location.href = "https://linktr.ee/yourwayhomestockholm";
+//   if (navigator.userAgent.includes("Instagram")) {
+//     window.location.href = "https://linktr.ee/yourwayhomestockholm";
+//   } else {
+//     window.location.href =
+//       "https://script.google.com/macros/s/AKfycby03ipJ4q6tdydTDYQjicbadMzp5vqBJPwcqXc4WcU7-vRA5qN7APLN6CJBLRdeAkHmxg/exec";
+//   }
 // });
 
 btnEl.addEventListener("click", function () {
   if (navigator.userAgent.includes("Instagram")) {
     window.location.href = "https://linktr.ee/yourwayhomestockholm";
   } else {
-    window.location.href =
-      "https://script.google.com/macros/s/AKfycby03ipJ4q6tdydTDYQjicbadMzp5vqBJPwcqXc4WcU7-vRA5qN7APLN6CJBLRdeAkHmxg/exec";
+    window.location.href = "https://tinyurl.com/ywhstockholm";
   }
 });
