680e58ca20398d12edb3815cce4aa95fd428cba0
Carmela Pippa <carmelapippa@gmail.com>
2024-08-06 17:52:22 +0200

    script

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ let btnEl = document.querySelector("button");
 // });
 
 btnEl.addEventListener("click", function () {
-  console.log("Button clicked");
-  window.location.href =
-    "https://script.google.com/macros/s/AKfycbxYcvheGYZnYOvjiZBvDiBRfa8icB0l8M0snJXufhcMJz4w3JhtZch510MKUSYdVQmq8w/exec";
+  if (navigator.userAgent.includes("Instagram")) {
+    window.location.href = "https://linktr.ee/yourwayhomestockholm";
+  } else {
+    window.location.href =
+      "https://script.google.com/macros/s/AKfycby03ipJ4q6tdydTDYQjicbadMzp5vqBJPwcqXc4WcU7-vRA5qN7APLN6CJBLRdeAkHmxg/exec";
+  }
 });
