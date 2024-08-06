1c29fd0575aba0e5a1836360177e7133fffb6372
Carmela Pippa <carmelapippa@gmail.com>
2024-08-06 14:28:24 +0200

    LINK

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let btnEl = document.querySelector("button");
 
+// btnEl.addEventListener("click", function () {
+//   console.log("Button clicked");
+//   window.location.href = "https://linktr.ee/yourwayhomestockholm";
+// });
+
 btnEl.addEventListener("click", function () {
   console.log("Button clicked");
-  window.location.href = "https://tinyurl.com/yourwayhome";
+  window.location.href = "https://linktr.ee/yourwayhomestockholm";
 });
