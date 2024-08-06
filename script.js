8c2d1805add2399c3b442fd5ca8b03442503ae42
Carmela Pippa <carmelapippa@gmail.com>
2024-08-06 18:12:27 +0200

    fixed link

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let btnEl = document.querySelector("button");
 
-// btnEl.addEventListener("click", function () {
-//   if (navigator.userAgent.includes("Instagram")) {
-//     window.location.href = "https://linktr.ee/yourwayhomestockholm";
-//   } else {
-//     window.location.href =
-//       "https://script.google.com/macros/s/AKfycby03ipJ4q6tdydTDYQjicbadMzp5vqBJPwcqXc4WcU7-vRA5qN7APLN6CJBLRdeAkHmxg/exec";
-//   }
-// });
-
 btnEl.addEventListener("click", function () {
   if (navigator.userAgent.includes("Instagram")) {
-    window.location.href = "https://tinyurl.com/ywhstockholm";
+    window.location.href = "https://linktr.ee/yourwayhomestockholm";
   } else {
     window.location.href =
       "https://script.google.com/macros/s/AKfycby03ipJ4q6tdydTDYQjicbadMzp5vqBJPwcqXc4WcU7-vRA5qN7APLN6CJBLRdeAkHmxg/exec";
   }
 });
+
+// btnEl.addEventListener("click", function () {
+//   if (navigator.userAgent.includes("Instagram")) {
+//     window.location.href = "https://tinyurl.com/ywhstockholm";
+//   } else {
+//     window.location.href =
+//       "https://script.google.com/macros/s/AKfycby03ipJ4q6tdydTDYQjicbadMzp5vqBJPwcqXc4WcU7-vRA5qN7APLN6CJBLRdeAkHmxg/exec";
+//   }
+// });
