ec82410d7707cd2fa920afa7291175b64245cad4
Carmela Pippa <carmelapippa@gmail.com>
2024-08-06 18:10:33 +0200

    link changed

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ let btnEl = document.querySelector("button");
 
 btnEl.addEventListener("click", function () {
   if (navigator.userAgent.includes("Instagram")) {
-    window.location.href = "https://linktr.ee/yourwayhomestockholm";
-  } else {
     window.location.href = "https://tinyurl.com/ywhstockholm";
+  } else {
+    window.location.href =
+      "https://script.google.com/macros/s/AKfycby03ipJ4q6tdydTDYQjicbadMzp5vqBJPwcqXc4WcU7-vRA5qN7APLN6CJBLRdeAkHmxg/exec";
   }
 });
