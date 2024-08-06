c2199e00ca0ae7feceaf006e4279a83ade0eb584
Carmela Pippa <carmelapippa@gmail.com>
2024-08-06 14:45:56 +0200

    script

--- script.js
+++ script.js
@@ -7,5 +7,6 @@ let btnEl = document.querySelector("button");
 
 btnEl.addEventListener("click", function () {
   console.log("Button clicked");
-  window.location.href = "https://linktr.ee/yourwayhomestockholm";
+  window.location.href =
+    "https://script.google.com/macros/s/AKfycbzhy0khgKBx8w4raxWWvZphESO9PpF_Ip9DBcMOo9WpJivtCkL-PexqMjiDsd7wAM1S5w/exec";
 });
