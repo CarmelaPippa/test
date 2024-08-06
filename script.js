a36594d8505c40107f2fa8ccca0ac705b8862fb5
Carmela Pippa <carmelapippa@gmail.com>
2024-08-06 14:10:24 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let btnEl = document.querySelector("button");
+
+btnEl.addEventListener("click", function () {
+  console.log("Button clicked");
+  window.location.href = "https://tinyurl.com/yourwayhome";
+});
