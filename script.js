9198c85864343465f746a73a0e2c88bc4242a039
Carmela Pippa <carmelapippa@gmail.com>
2024-08-06 15:33:08 +0200

    change the link

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ let btnEl = document.querySelector("button");
 btnEl.addEventListener("click", function () {
   console.log("Button clicked");
   window.location.href =
-    "https://script.google.com/macros/s/AKfycbzhy0khgKBx8w4raxWWvZphESO9PpF_Ip9DBcMOo9WpJivtCkL-PexqMjiDsd7wAM1S5w/exec";
+    "https://script.google.com/macros/s/AKfycbxYcvheGYZnYOvjiZBvDiBRfa8icB0l8M0snJXufhcMJz4w3JhtZch510MKUSYdVQmq8w/exec";
 });
